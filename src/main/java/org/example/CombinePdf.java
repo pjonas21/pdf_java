38784b83482bde7a0d3b0465156effcc5885be20
Paulo Jonas <pjonas21@gmail.com>
2024-10-09 15:45:35 -0300

    add desktop interface

--- src/main/java/org/example/CombinePdf.java
+++ src/main/java/org/example/CombinePdf.java
@@ -5,27 +5,34 @@ import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 public class CombinePdf {
     private final String caminhoPasta;
-    private final PDFMergerUtility merger;
+    private final  PDFMergerUtility merger;
+    private final String newFileName;
 
-    public CombinePdf(String caminhoPasta, PDFMergerUtility merger) {
+    public CombinePdf(String caminhoPasta, PDFMergerUtility merger, String newFileName) {
         this.caminhoPasta = caminhoPasta;
         this.merger = merger;
+        this.newFileName = newFileName;
     }
 
     public void combinePdf() throws FileNotFoundException {
         File pasta = new File(this.caminhoPasta);
 
         if(pasta.isDirectory()) {
-            List<File> pdfFiles = List.of(Objects.requireNonNull(pasta.listFiles()));
+        	File[] pastaArquivos = Objects.requireNonNull(pasta.listFiles());
+            List<File> pdfFiles = new ArrayList<>(Arrays.asList(pastaArquivos));
+            Collections.sort(pdfFiles);
             for (File arq : pdfFiles) {
                 this.merger.addSource(new File(String.valueOf(arq)));
             }
-            this.merger.setDestinationFileName("pdf_combinado.pdf");
+            this.merger.setDestinationFileName(this.newFileName + ".pdf");
             try {
                 this.merger.mergeDocuments(null);
                 System.out.println("PDF mesclado com sucesso.");
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,115 @@ package org.example;
 
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
 
-import java.io.FileNotFoundException;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Objects;
+
+public class Main extends Application {
+	
+	private TextField folderPathField;
+	private TextField fileName;
+	
+	private PDFMergerUtility merger;
+	private CombinePdf combinador;
 
     public static void main(String[] args) throws FileNotFoundException {
-
-        PDFMergerUtility merger = new PDFMergerUtility();
-        String caminhoPasta = "./arquivos";
-        CombinePdf combinador = new CombinePdf(caminhoPasta, merger);
-        combinador.combinePdf();
+    	
+    	launch(args);
 
     }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		setAppIcon(primaryStage);
+		
+		folderPathField = new TextField();
+		folderPathField.setPromptText("Selecione a pasta dos arquivos");
+		folderPathField.setPrefWidth(250);
+		
+		Alert alertSuccess = new Alert(AlertType.INFORMATION);
+		Alert alertError = new Alert(AlertType.ERROR);
+		
+		Button selectFolderButton = new Button("Selecionar pasta");
+		selectFolderButton.setOnAction(event -> selectFolderPath(primaryStage));
+		
+		Button actionButton = new Button("Mesclar PDFs");
+		actionButton.setOnAction(e -> {
+			try {
+				mergeActionButton();
+				alertSuccess.setTitle("Sucesso");
+				alertSuccess.setContentText("PDFs mesclados com sucesso.");
+				alertSuccess.show();
+				folderPathField.clear();
+				fileName.clear();
+			} catch (FileNotFoundException e1) {
+				alertError.setTitle("Falha");
+				alertError.setContentText("Erro ao processar arquivos. Arquivos corrompidos/inexistentes.");
+				alertError.show();
+			}
+		});
+
+		fileName = new TextField();
+		fileName.setPromptText("Nome do novo arquivo.");
+		fileName.setMaxWidth(150);
+		
+		HBox hLayout = new HBox(10, folderPathField, selectFolderButton);
+		VBox vLayout = new VBox(10, hLayout, fileName, actionButton);
+		vLayout.setPadding(new Insets(15));
+		
+		Scene scene = new Scene(vLayout, 450, 150);
+		
+		primaryStage.setTitle("PDF Merger");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		
+	}
+
+	private void mergeActionButton() throws FileNotFoundException {
+		String folderPath = folderPathField.getText();
+		if(!folderPath.isEmpty()) {
+			mergeAction(folderPath, this.fileName.getText());
+		} else {
+			System.out.println("Nenhuma pasta selecionada");
+		}
+	}
+	
+	private void mergeAction(String path, String newFileName) throws FileNotFoundException {
+		merger = new PDFMergerUtility();
+		combinador = new CombinePdf(path, merger, newFileName);
+		combinador.combinePdf();
+	}
+
+	private void selectFolderPath(Stage stage) {
+		DirectoryChooser chooser = new DirectoryChooser();
+		chooser.setTitle("Selecione uma pasta");
+		File selectedDirectory = chooser.showDialog(stage);
+		if(selectedDirectory != null) {
+			folderPathField.setText(selectedDirectory.getAbsolutePath());
+			System.out.println(folderPathField.getText());
+		}
+	}
+	
+	private void setAppIcon(Stage stage) {
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/pdf.png")));
+        stage.getIcons().add(icon);
+    }
+	
+	
 
 }
